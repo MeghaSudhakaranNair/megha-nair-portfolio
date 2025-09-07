19982d8923b627d3cccccbd62e7a0df15d96dc72
MeghaSudhakaranNair <megha99.sudhakaran@gmail.com>
2025-09-07 15:35:02 -0400

    added more content for experience

--- app/components/MainCards.tsx
+++ app/components/MainCards.tsx
@@ -1,7 +1,16 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
-import { Box, Card, CardContent, Typography } from "@mui/material";
+import React, { useEffect, useRef, useState } from "react";
+import {
+  Box,
+  Card,
+  CardActionArea,
+  CardContent,
+  Collapse,
+  IconButton,
+  Typography,
+} from "@mui/material";
+import CloseIcon from "@mui/icons-material/Close";
 import AOS from "aos";
 import "aos/dist/aos.css";
 interface Project {
@@ -9,6 +18,7 @@ interface Project {
   from: string;
   to: string;
   image: string;
+  feature: string[];
 }
 
 interface MainCardsProps {
@@ -17,7 +27,10 @@ interface MainCardsProps {
 
 const MainCards = ({ projects }: MainCardsProps) => {
   const [currentTheme, setCurrentTheme] = useState<string | null>(null);
-
+  const [expandedCard, setExpandedCard] = useState<number | null>(null);
+  const [isExpand, setIsExpanded] = useState<boolean>(false);
+  const expandedCardRef = useRef<HTMLDivElement>(null);
+  const [componentKey, setComponentKey] = useState(0);
   // Set theme on mount and observe theme changes dynamically
   useEffect(() => {
     // Function to update theme
@@ -25,6 +38,7 @@ const MainCards = ({ projects }: MainCardsProps) => {
       const theme = document.documentElement.getAttribute("data-theme");
       setCurrentTheme(theme);
       console.log("Current theme:", theme); // Log the current theme
+      setComponentKey((prevKey) => prevKey + 1);
     };
 
     // Initialize observer to watch for changes to the `data-theme` attribute
@@ -84,7 +98,7 @@ const MainCards = ({ projects }: MainCardsProps) => {
 
     // Cleanup observer when component unmounts
     return () => observer.disconnect();
-  }, []);
+  }, [isExpand]);
 
   // Define background and text color based on the current theme
   const backgroundColor = currentTheme === "dark" ? "#0a0a0a" : "#ffffff";
@@ -100,9 +114,29 @@ const MainCards = ({ projects }: MainCardsProps) => {
     currentTheme === "dark"
       ? "0 10px 24px rgba(0,0,0,0.45)"
       : "0 12px 28px rgba(22,34,51,0.18)";
+  const handleExpandClick = (index: number) => {
+    setExpandedCard(index);
+    setIsExpanded(true);
+    if (window.innerWidth <= 900) {
+      // Only scroll on smaller screens
+      setTimeout(() => {
+        if (expandedCardRef.current) {
+          expandedCardRef.current.scrollIntoView({
+            behavior: "smooth",
+            block: "start",
+          });
+        }
+      }, 500); // The delay should match the Collapse timeout
+    }
+  };
 
+  const expandedProject =
+    expandedCard !== null
+      ? projects[expandedCard]
+      : { title: "", from: "", to: "", image: "", feature: [] };
   return (
     <Box
+      key={componentKey}
       sx={{
         display: "flex",
         flexWrap: "wrap",
@@ -124,25 +158,18 @@ const MainCards = ({ projects }: MainCardsProps) => {
           }}
         >
           <Card
+            onClick={() => handleExpandClick(index)}
             className="fade-card"
             sx={{
               width: 300,
 
-              //   #a4b3ff26
-              // backgroundColor:
-              //   currentTheme === "dark"
-              //     ? "rgba(31, 31, 31, 0.6)" // dark translucent
-              //     : "rgba(21, 128, 248, 0.1)", // light translucent
               backgroundColor:
                 currentTheme === "dark"
                   ? "rgba(17,17,17,0.6)"
                   : "rgba(255,255,255,0.6)",
 
-              // ✅ Gradients must go in backgroundImage (not backgroundColor)
-
-              // Optional: blend the gradient with the base color a bit more
               backgroundBlendMode: "overlay",
-
+              cursor: "pointer",
               backdropFilter: "blur(10px)",
               WebkitBackdropFilter: "blur(10px)",
               color: currentTheme === "dark" ? "#fff" : "#000",
@@ -154,6 +181,25 @@ const MainCards = ({ projects }: MainCardsProps) => {
                 transform: "translateY(-2px)",
                 boxShadow: shadowHover,
               },
+
+              "&::after": {
+                content: '""',
+                position: "absolute",
+                inset: 0,
+                opacity: 0,
+                borderRadius: "10px",
+                transition: "opacity 200ms ease",
+                // Define your hover gradient here
+                background:
+                  currentTheme === "dark"
+                    ? "linear-gradient(135deg, rgba(96,165,250,0.12), rgba(59,130,246,0.10) 40%, rgba(221,214,254,0.16) 85%)"
+                    : "linear-gradient(135deg, rgba(21,128,248,0.10), rgba(147,197,253,0.12) 45%, rgba(221,214,254,0.16) 90%)",
+              },
+              // The hover state that changes the pseudo-element's opacity
+              "&:hover::after": {
+                opacity: 0.5,
+              },
+
               // padding: "25px 15px 15px 15px",
             }}
             data-aos="fade-up"
@@ -170,39 +216,175 @@ const MainCards = ({ projects }: MainCardsProps) => {
                 borderRadius: "10px 10px 0px 0px",
               }}
             />
-
-            <CardContent
-              sx={{
-                background:
-                  currentTheme === "dark"
-                    ? "linear-gradient(180deg, rgba(0,0,0,0) 0%, rgba(17,17,17,0.5) 20%, rgba(96,165,250,0.10) 100%)"
-                    : "linear-gradient(180deg, rgba(255,255,255,0) 0%, rgba(255,255,255,0.6) 20%, rgba(21,128,248,0.08) 100%)",
-              }}
-            >
-              <Typography
-                variant="h6"
+            <CardActionArea>
+              <CardContent
                 sx={{
-                  color: textColor,
-                  textAlign: "left",
-                  marginBottom: "2px",
+                  borderRadius: "none !important",
+                  background:
+                    currentTheme === "dark"
+                      ? "linear-gradient(180deg, rgba(0,0,0,0) 0%, rgba(17,17,17,0.5) 20%, rgba(96,165,250,0.10) 100%)"
+                      : "linear-gradient(180deg, rgba(255,255,255,0) 0%, rgba(255,255,255,0.6) 20%, rgba(21,128,248,0.08) 100%)",
                 }}
               >
-                {project.title}
-              </Typography>
-              <Typography
-                variant="body2"
-                sx={{
-                  color: textColor,
-                  textAlign: "left",
-                  marginBottom: "5px",
-                }}
-              >
-                {project.from} - {project.to}
-              </Typography>
-            </CardContent>
+                <Typography
+                  variant="h6"
+                  sx={{
+                    color: textColor,
+                    textAlign: "left",
+                    marginBottom: "2px",
+                  }}
+                >
+                  {project.title}
+                </Typography>
+                <Typography
+                  variant="body2"
+                  sx={{
+                    color: textColor,
+                    textAlign: "left",
+                    marginBottom: "5px",
+                  }}
+                >
+                  {project.from} - {project.to}
+                </Typography>
+              </CardContent>
+            </CardActionArea>
           </Card>
         </Box>
       ))}
+      <Collapse
+        className="fade-card"
+        in={isExpand}
+        timeout={{ enter: 500, exit: 500 }}
+        unmountOnExit
+      >
+        <Box
+          ref={expandedCardRef}
+          sx={{
+            position: "relative",
+            overflow: "hidden",
+            width: "100%",
+            maxWidth: "1080px",
+            margin: "0 auto", // Center the box
+            p: 4,
+            borderRadius: "10px",
+            boxShadow: shadow,
+            backgroundColor:
+              currentTheme === "dark"
+                ? "rgba(140,183,242,0.1)"
+                : "rgba(187,211,247,0.1)",
+            backgroundBlendMode: "overlay",
+            backdropFilter: "blur(10px)",
+            WebkitBackdropFilter: "blur(10px)",
+            color: currentTheme === "dark" ? "#fff" : "#000",
+            transition: "transform 200ms ease, box-shadow 200ms ease",
+            "@keyframes draw-border": {
+              "0%": {
+                width: "0%",
+                height: "2px",
+                top: 0,
+                right: 0,
+              },
+              "25%": {
+                width: "100%",
+                height: "2px",
+                top: 0,
+                right: 0,
+              },
+              "50%": {
+                width: "2px",
+                height: "100%",
+                top: 0,
+                right: 0,
+              },
+              "75%": {
+                width: "100%",
+                height: "2px",
+                bottom: 0,
+                left: 0,
+              },
+              "100%": {
+                width: "2px",
+                height: "100%",
+                top: 0,
+                left: 0,
+              },
+            },
+
+            "&:hover": {
+              boxShadow: shadowHover,
+            },
+
+            "&:before": {
+              content: '""',
+              position: "absolute",
+              inset: 0,
+              borderRadius: "10px", // Use a static value or a variable
+              padding: "1px",
+              animation: "aurora 5s linear infinite",
+
+              background:
+                currentTheme === "dark"
+                  ? "linear-gradient(100deg,rgba(0,0,0,0) 0%, rgb(65, 134, 246) 20%, rgb(179, 202, 248) 40%, rgb(230, 238, 251) 60%, rgba(0,0,0,0) 100%)"
+                  : "linear-gradient(100deg, rgb(174, 197, 242) 0%, rgb(179, 202, 248) 20%, rgb(255,255,255) 40%, rgba(255,255,255) 60%, rgba(255,255,255) 100%)",
+
+              backgroundSize: "300% 300%",
+
+              WebkitMask:
+                "linear-gradient(#fff 0 0) content-box, linear-gradient(#fff 0 0)",
+              WebkitMaskComposite: "xor",
+              maskComposite: "exclude",
+              // Z-index to place the border behind the content
+              zIndex: -1,
+              "@keyframes aurora": {
+                "0%": {
+                  backgroundPosition: "0% 50%",
+                },
+                "100%": {
+                  backgroundPosition: "200% 50%",
+                },
+              },
+            },
+          }}
+        >
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+              mb: 2,
+            }}
+          >
+            <Typography
+              variant="h5"
+              sx={{ color: textColor, mb: 2, fontWeight: "bold" }}
+            >
+              {expandedProject?.title}
+            </Typography>
+            <IconButton
+              onClick={() => setIsExpanded(false)}
+              sx={{ color: textColor }}
+            >
+              <CloseIcon />
+            </IconButton>
+          </Box>
+          <Typography
+            variant="subtitle1"
+            sx={{ color: textColor, mb: 1, fontWeight: "bold" }}
+          >
+            Key Contributions:
+          </Typography>
+          <Box
+            component="ul"
+            sx={{ pl: 2, listStyleType: "disc", color: textColor }}
+          >
+            {expandedProject?.feature.map((feature: any, i: any) => (
+              <Box component="li" key={i} sx={{ mb: 1 }}>
+                <Typography variant="body2">{feature}</Typography>
+              </Box>
+            ))}
+          </Box>
+        </Box>
+      </Collapse>
     </Box>
   );
 };
--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -13,18 +13,37 @@ const Projects = () => {
       from: "June 2024",
       to: "Present",
       image: "/blusvn.jpeg", // Add your image path here
+      feature: [
+        "•Led a team of 3 developers to design and develop the companyʼs official website and app using Next.js, Material UI, and Capacitor, collaborating in Figma to deliver a responsive, accessible, and brand-aligned experience across web and mobile platforms.",
+        "Developed UI and integrated tRPC APIs for core modules such as product management, order management, and chat integration.",
+        "Enhanced authentication by resolving SSO issues and implementing biometric authentication with Capacitor Biometrics, improving security, streamlining access, and enabling faster logins.",
+        "Optimized LCP by implementing lazy-loading and asset preloading, boosting Lighthouse scores by 15% and improving load times. •Implemented secure private and public routing with next/navigation, improving platform security, structured navigation, and seamless user access across pages.",
+      ],
     },
     {
       title: "Software Engineer",
       from: "Aug 2024",
       to: "May 2023",
       image: "/interbiz.jpeg", // Add your image path here
+      feature: [
+        "Collaborated with clients to gather requirements for an IoT platform, delivering solutions that enhanced satisfaction.",
+        "Optimized the heartbeat schema to reduce request-response frequency, improving cost efficiency and streamlining device communication. •Implemented an aggregator schema in Cosmos DB, reducing UI fetch time by 30% and enhancing system performance.",
+        "Developed a responsive UI in Angular, implementing features like scheduling, user management, and device monitoring.",
+        "Integrated dynamic forms and time zone management for scheduling, ensuring accurate device operations and an intuitive user experience. •Built a feature to request, store, and retrieve device log files using blob storage, enabling efficient downloads reducing redundant requests.",
+      ],
     },
     {
       title: "Software Developer",
       from: "July 2020",
       to: "July 2021",
       image: "/hailabs.png", // Add your image path here
+      feature: [
+        "Conducted research on the most recent AI trends and developed a curriculum with over 27 modules and 12 projects for students in grades 4 through 9.",
+        "Facilitated cross-functional collaboration between technical, pedagogy, and ML teams to ensure the seamless integration of AI technologies for an edtech platform for students learn data science and ML in a gamified manner.",
+        "Refined ML model accuracy by leveraging Python, TensorFlow, and Keras to build robust data pipelines and optimize algorithms for an intuitive student learning experience.",
+        "Engineered and deployed a suite of AI-powered games for a student learning platform, integrating pre-trained FaceNet and BERT models to teach computer vision and natural language processing concepts in a gamified and intuitive way.",
+        "Managed collaborative development workflows and maintained code quality by implementing version control with GitHub.",
+      ],
     },
     // Add more projects as needed
   ];
